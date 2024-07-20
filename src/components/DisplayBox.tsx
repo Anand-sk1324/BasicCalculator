f6b8196d5ec6b1e2b2c2572e744fe81c018f6e77
Anand-sk1324 <anand.sk1324@gmail.com>
2024-07-20 18:04:08 +0530

    build v1

--- src/components/DisplayBox.tsx
+++ src/components/DisplayBox.tsx
@@ -1,10 +1,12 @@
-import { useStore } from "zustand";
 import useDisplay from "../hooks/useDisplay";
 const DisplayBox = () => {
     const { displayValue } = useDisplay();
     return (
-        <div className=" w-full h-full border border-black px-2 col-span-3 flex justify-end items-center bg-slate-200 ">
-            <span> {displayValue ? displayValue : '0'} </span>
+        <div className=" w-full h-full border border-black px-2 col-span-3 flex justify-end items-center bg-slate-200 rounded-xl ">
+            <p className="block w-32 overflow-hidden whitespace-nowrap text-ellipsis text-right">
+                {" "}
+                {displayValue ? displayValue : "0"}{" "}
+            </p>
         </div>
     );
 };
